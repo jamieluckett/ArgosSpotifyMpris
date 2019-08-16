7c34b1279ff6ef0b34c045127ad992e7b6a9f3a6
James Luckett <james.luckett@opsview.com>
2019-08-16 15:38:39 +0100

    Added argos_print command to generate lines with kwargs

--- spotify.r.1s+.py
+++ spotify.r.1s+.py
@@ -20,39 +20,55 @@ PLAYBACK_STATUS_ICON_MAPPING = {
 UNCLICKABLE_COLOUR = "#888888"
 
 
+def argos_print(body, **kwargs):
+    if kwargs:
+        arguments = " ".join([arg_format(arg) for arg in kwargs.items()])
+        print("{0} | {1}".format(body, arguments))
+    else:
+        print(body)
+
+
+def print_argos_separator():
+    print("---")
+
+
+def arg_format(arg):
+    name = arg[0]
+    value = arg[1]
+    if ' ' in value:
+        return "{0}='{1}'".format(name, value)
+    return "{0}={1}".format(name, value)
+
+
 def get_spotify_object():
     bus = SessionBus()
     return bus.get(BUS_NAME, OBJECT_PATH)
 
 
-def print_unclickable(text):
-    print("{0} | color={1}".format(text, UNCLICKABLE_COLOUR))
-
-
 def print_control_menu(playback_status):
     if playback_status in ["Stopped", "Paused"]:
-        print("<b>Play</b>/Pause | "
-              "iconName=media-playback-start "
-              "bash='dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify "
-              "/org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause' "
-              "terminal=false")
+        argos_print("<b>Play</b>/Pause",
+                    iconName="media-playback-start",
+                    bash="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify "
+                         "/org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause",
+                    terminal="false")
     elif playback_status == "Playing":
-        print("Play/<b>Pause</b> | "
-              "iconName=media-playback-pause "
-              "bash='dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify "
-              "/org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause' "
-              "terminal=false")
+        argos_print("Play/<b>Pause</b>",
+                    iconName="media-playback-pause",
+                    bash="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify "
+                         "/org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause",
+                    terminal="false")
 
-    print("Previous | "
-          "iconName=media-skip-backward "
-          "bash='dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify "
-          "/org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous' "
-          "terminal=false")
-    print("Next | "
-          "iconName=media-skip-forward "
-          "bash='dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify "
-          "/org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next' "
-          "terminal=false")
+    argos_print("Previous",
+                iconName="media-skip-backward",
+                bash="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify "
+                     "/org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Previous",
+                terminal="false")
+    argos_print("Next",
+                iconName="media-skip-forward",
+                bash="dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify "
+                     "/org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Next",
+                terminal="false")
 
 
 if __name__ == "__main__":
@@ -68,30 +84,32 @@ if __name__ == "__main__":
             song_title = metadata['xesam:title']
             art_url = metadata['mpris:artUrl']
             album_name = metadata['xesam:album']
-            print("{0} - {1} | iconName={2} useMarkup=false unescape=true".format(
-                primary_artist, song_title, PLAYBACK_STATUS_ICON_MAPPING[playback_status]))
-            print("---")
-            print_unclickable("Song Title: {0}".format(song_title))
-            print_unclickable("Album: {0}".format(album_name))
+            argos_print("{0} - {1}".format(primary_artist, song_title),
+                        iconName=PLAYBACK_STATUS_ICON_MAPPING[playback_status],
+                        useMarkup="false",
+                        unescape="true")
+            print_argos_separator()
+            argos_print("Song Title: {0}".format(song_title), color=UNCLICKABLE_COLOUR)
+            argos_print("Album: {0}".format(album_name), color=UNCLICKABLE_COLOUR)
             if len(artist_list) > 1:
                 # If only the plural of Artist was Artist...
                 # Spotify never seems to actually return more than 1 artist through mpris though but maybe one day
-                print_unclickable("Artists: {0}".format(", ".join(artist_list)))
+                argos_print("Artists: {0}".format(", ".join(artist_list)), color=UNCLICKABLE_COLOUR)
             else:
-                print_unclickable("Artist: {0}".format(", ".join(artist_list)))
+                argos_print("Artist: {0}".format(", ".join(artist_list)), color=UNCLICKABLE_COLOUR)
             print_control_menu(playback_status)
         else:
-            print("Nothing Playing | iconName=media-playback-stop")
+            argos_print("Nothing Playing", iconName="media-playback-stop")
 
     except GError as e:
         # A GError indicates that (most likely) Spotify isn't open.
-        print("Spotify isn't open! | iconName=media-playback-stop")
-        print("---")
-        print("Open Spotify | iconName={0} bash='spotify U%' terminal=false".format(SPOTIFY_ICON_NAME))
+        argos_print("Spotify isn't open!", iconName="media-playback-stop")
+        print_argos_separator()
+        argos_print("Open Spotify", iconName=SPOTIFY_ICON_NAME, bash="spotify U%", terminal="false")
     except Exception as e:
         # Catch generic exception here so it can be displayed in the dropdown
-        print("Something went wrong! | iconName=dialog-warning")
-        print("---")
-        print("<b>Exception Details:</b>")
+        argos_print("Something went wrong!", iconName="dialog-warning")
+        print_argos_separator()
+        argos_print("<b>Exception Details:</b>")
         tb = traceback.format_exc()
-        print(tb.replace("\n", "\\n"), "| font=monospace useMarkup=false unescape=true")
+        argos_print(tb.replace("\n", "\\n"), font="monospace", useMarkup="false", unescape="true")
